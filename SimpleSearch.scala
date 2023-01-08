c2f260c5c75f07f198718026e43a950927e5c377
takanoria0612 <ta.kanorioh.my.zsh@gmail.com>
2023-01-09 00:25:18 +0900

    first commit

---
+++ SimpleSearch.scala
@@ -0,0 +1,25 @@
+object SimpleSearch extends App {
+  val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
+  val pattern = "ドワンゴ".toSeq
+  val matchIndexes = search(text, pattern)
+
+  def search(text: Seq[Char], pattern: Seq[Char]):Seq[Int]={
+    var matchIndexes = Seq[Int]()
+    for (i <- 0 to text.length - 1) {
+        val partText = text.slice(i, i+ pattern.length)
+        println(partText)
+        if (isMatch(partText, pattern)) matchIndexes = matchIndexes :+ i
+    }
+    matchIndexes
+  }
+
+def isMatch(textPart: Seq[Char], pattern: Seq[Char]): Boolean = {
+    var isMatch = true;
+    for (i <- 0 to pattern.length - 1) {
+        if (textPart.length < pattern.length || textPart(i)!= pattern(i)) isMatch= false
+    }
+    isMatch
+}
+
+  println(s"出現位置: ${matchIndexes}")
+}
